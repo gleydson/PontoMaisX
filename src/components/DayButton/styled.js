e7974551bc9dad08280d6a7027fd329c04a7bce5
Gleydson Rodrigues <gleydsonsr@gmail.com>
2019-11-18 18:06:00 -0300

    Refactor styles

--- src/components/DayButton/styled.js
+++ src/components/DayButton/styled.js
@@ -1,17 +1,24 @@
 import styled from 'styled-components/native';
-import { Text } from 'react-native';
 
-export const Container = styled.View.attrs(({ selected }) => selected && ({
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  }))`
-  background-color: ${({ selected }) => selected ? '#3AB6AC' : 'rgba(255, 255, 255, 0.3)'};
+import { colors, fonts } from '~/styles';
+
+export const Button = styled.TouchableWithoutFeedback``;
+
+export const Container = styled.View.attrs(
+  ({ selected }) =>
+    selected && {
+      shadowColor: colors.black,
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+    }
+)`
+  background-color: ${({ selected }) =>
+    selected ? colors.secondary : 'rgba(255, 255, 255, 0.3)'};
   padding: 5px;
   width: 45px;
   height: 55px;
@@ -20,15 +27,15 @@ export const Container = styled.View.attrs(({ selected }) => selected && ({
 `;
 
 export const DayNumber = styled.Text`
-  font-size: 20px;
-  color: #fff;
+  font-size: ${fonts.big};
+  color: ${colors.white};
   font-weight: bold;
   text-align: center;
 `;
 
 export const DayWeek = styled.Text`
-  font-size: 12px;
-  color: #fff;
+  font-size: ${fonts.small};
+  color: ${colors.white};
   text-transform: capitalize;
   text-align: center;
 `;
