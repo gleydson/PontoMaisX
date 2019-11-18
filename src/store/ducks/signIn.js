dbffaab42880ee284c5b14a275a22a6b00d1c270
Gleydson Rodrigues <gleydsonsr@gmail.com>
2019-11-18 18:06:50 -0300

    Add new infos in state

--- src/store/ducks/signIn.js
+++ src/store/ducks/signIn.js
@@ -15,7 +15,13 @@ export const { Types, Creators } = createActions({
 const INITIAL_STATE = Immutable({
   isLogged: false,
   isShowCheckAnimation: false,
+  uid: '',
+  accessToken: '',
+  client: '',
 });
+
+const loginSuccess = (state = INITIAL_STATE, { uid, accessToken, client }) =>
+  state.merge({ uid, accessToken, client });
 
 const sessionSuccess = (state = INITIAL_STATE) =>
   state.merge({ isShowCheckAnimation: true });
@@ -24,6 +30,7 @@ const setIsLogged = (state = INITIAL_STATE, { status }) =>
   state.merge({ isLogged: status });
 
 export default createReducer(INITIAL_STATE, {
+  [Types.LOGIN_SUCCESS]: loginSuccess,
   [Types.SESSION_SUCCESS]: sessionSuccess,
   [Types.SET_IS_LOGGED]: setIsLogged,
 });
@@ -31,4 +38,7 @@ export default createReducer(INITIAL_STATE, {
 export const Selectors = {
   isLogged: state => state.signIn.isLogged,
   isShowCheckAnimation: state => state.signIn.isShowCheckAnimation,
+  uid: state => state.signIn.uid,
+  accessToken: state => state.signIn.accessToken,
+  client: state => state.signIn.client,
 };
