a0da1b6dd8314e0c841fa64669372d1cc77228bc
Gleydson Rodrigues <gleydsonsr@gmail.com>
2019-11-18 18:07:20 -0300

    Add more ducks to listen

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -3,12 +3,18 @@ import { resettableReducer } from 'reduxsauce';
 
 import company from './company';
 import employee from './employee';
-import SignIn from './signIn';
+import settings from './settings';
+import shimmerUi from './shimmerUi';
+import signIn from './signIn';
+import workDay from './workDay';
 
 const resettable = resettableReducer('RESET_AFTER_LOGOUT');
 
 export default combineReducers({
-  signIn: resettable(SignIn),
+  signIn: resettable(signIn),
   company: resettable(company),
   employee: resettable(employee),
+  workDay: resettable(workDay),
+  shimmerUi: resettable(shimmerUi),
+  settings,
 });
