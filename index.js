fea3031a169b03676b102377215fdd39a3eda463
Gleydson Rodrigues <gleydsonsr@gmail.com>
2019-11-21 13:57:43 -0300

    Transfer provider and persistor to entry point

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { name as appName } from './app.json';
 import App from './src';
+import { store, persistor } from './src/store';
 
-AppRegistry.registerComponent(appName, () => App);
+function PontoMaisX() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => PontoMaisX);
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import useAppState from 'react-native-appstate-hook';
+import { useSelector, useDispatch } from 'react-redux';
 
 import '~/config/Reactotron';
 import '~/config/StatusBar';
 import getPersistenceFunctions from '~/config/PersistNavigation';
 import Routes from '~/routes';
 import NavigationService from '~/services/navigation';
-import { store, persistor } from '~/store';
+import { Creators as SettingsCreators } from '~/store/ducks/settings';
+import { Selectors as SignInSelectors } from '~/store/ducks/signIn';
 
 export default function() {
+  const dispatch = useDispatch();
+  const isLogged = useSelector(SignInSelectors.isLogged);
+
+  useAppState({
+    onChange: newAppState =>
+      dispatch(SettingsCreators.handleStatusApp(newAppState === 'active')),
+  });
+
+  const RoutesWrapper = Routes(isLogged);
+
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Routes
-          ref={NavigationService.setNavigator}
-          {...getPersistenceFunctions()}
-        />
-      </PersistGate>
-    </Provider>
+    <RoutesWrapper
+      ref={NavigationService.setNavigator}
+      {...getPersistenceFunctions()}
+    />
   );
 }
